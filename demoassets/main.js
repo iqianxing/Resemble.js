0a0a1774baab71f8782a2504ab951fcc252307e3
qianxing <qianxingqianli@qq.com>
2017-06-29 22:23:16 +0800

    add socket.io in main.js

--- demoassets/main.js
+++ demoassets/main.js
@@ -1,76 +1,81 @@
-$(function(){
+$(function () {
 	var $target = $('#drop-zone');
 
-	function dropZone($target, onDrop){
-		$target.
-			bind('dragover', function(){
-				$target.addClass( 'drag-over' );
-				return false;
-			}).
-			bind("dragend", function () {
-				$target.removeClass( 'drag-over' );
-				return false;
-			}).
-			bind("dragleave", function () {
-				$target.removeClass( 'drag-over' );
-				return false;
-			}).
-			bind("drop", function(event) {
-				var file = event.originalEvent.dataTransfer.files[0];
-
-				event.stopPropagation();
-				event.preventDefault();
-
-				$target.removeClass( 'drag-over' );
-
-				var droppedImage = new Image();
-				var fileReader = new FileReader();
-
-				fileReader.onload = function (event) {
-					droppedImage.src = event.target.result;
-					$target.html(droppedImage);
-				};
-
-				fileReader.readAsDataURL(file);
-
-				onDrop(file);
-			});
+	var socket = io();
+	function sendCompareResult(result) {
+		socket.emit('compare finish', result);
 	}
 
-	dropZone($target, function(file){
+	function dropZone($target, onDrop) {
+		$target.
+		bind('dragover', function () {
+			$target.addClass('drag-over');
+			return false;
+		}).
+		bind("dragend", function () {
+			$target.removeClass('drag-over');
+			return false;
+		}).
+		bind("dragleave", function () {
+			$target.removeClass('drag-over');
+			return false;
+		}).
+		bind("drop", function (event) {
+			var file = event.originalEvent.dataTransfer.files[0];
 
-		resemble(file).onComplete(function(data){
+			event.stopPropagation();
+			event.preventDefault();
+
+			$target.removeClass('drag-over');
+
+			var droppedImage = new Image();
+			var fileReader = new FileReader();
+
+			fileReader.onload = function (event) {
+				droppedImage.src = event.target.result;
+				$target.html(droppedImage);
+			};
+
+			fileReader.readAsDataURL(file);
+
+			onDrop(file);
+		});
+	}
+
+	dropZone($target, function (file) {
+
+		resemble(file).onComplete(function (data) {
 			$('#image-data').show();
-			$('#red').css('width',data.red+'%');
-			$('#green').css('width',data.green+'%');
-			$('#blue').css('width',data.blue+'%');
-			$('#alpha').css('width',data.alpha+'%');
-			$('#brightness').css('width',data.brightness+'%');
-			$('#white').css('width',data.white+'%');
-			$('#black').css('width',data.black+'%');
+			$('#red').css('width', data.red + '%');
+			$('#green').css('width', data.green + '%');
+			$('#blue').css('width', data.blue + '%');
+			$('#alpha').css('width', data.alpha + '%');
+			$('#brightness').css('width', data.brightness + '%');
+			$('#white').css('width', data.white + '%');
+			$('#black').css('width', data.black + '%');
 		});
 
 	});
 
-	function onComplete(data){
+	function onComplete(data) {
 		var time = Date.now();
 		var diffImage = new Image();
 		diffImage.src = data.getImageDataUrl();
 
 		$('#image-diff').html(diffImage);
 
-		$(diffImage).click(function(){
+		$(diffImage).click(function () {
 			window.open(diffImage.src, '_blank');
 		});
 
 		$('.buttons').show();
 
-		if(data.misMatchPercentage == 0){
+		if (data.misMatchPercentage == 0) {
 			$('#thesame').show();
 			$('#diff-results').hide();
 		} else {
 			$('#mismatch').text(data.misMatchPercentage);
-			if(!data.isSameDimensions){
+			if (!data.isSameDimensions) {
 				$('#differentdimensions').show();
 			} else {
 				$('#differentdimensions').hide();
@@ -78,58 +83,55 @@ $(function(){
 			$('#diff-results').show();
 			$('#thesame').hide();
 		}
+
+		sendCompareResult(data)
 	}
 
 	var file1;
 	var file2;
 	var resembleControl;
 
-	dropZone($('#dropzone1'), function(file){
+	dropZone($('#dropzone1'), function (file) {
 		file1 = file;
-		if(file2){
+		if (file2) {
 			resembleControl = resemble(file).compareTo(file2).onComplete(onComplete);
 		}
 	});
 
-	dropZone($('#dropzone2'), function(file){
+	dropZone($('#dropzone2'), function (file) {
 		file2 = file;
-		if(file1){
+		if (file1) {
 			resembleControl = resemble(file).compareTo(file1).onComplete(onComplete);
 		}
 	});
 
 	var buttons = $('.buttons button');
 
-	buttons.click(function(){
+	buttons.click(function () {
 		var $this = $(this);
 
 		$this.parent('.buttons').find('button').removeClass('active');
 		$this.addClass('active');
 
-		if($this.is('#raw')){
+		if ($this.is('#raw')) {
 			resembleControl.ignoreNothing();
-		}
-		else
-		if($this.is('#less')){
+		} else
+		if ($this.is('#less')) {
 			resembleControl.ignoreLess();
 		}
-		if($this.is('#colors')){
+		if ($this.is('#colors')) {
 			resembleControl.ignoreColors();
-		}
-		else
-		if($this.is('#antialising')){
+		} else
+		if ($this.is('#antialising')) {
 			resembleControl.ignoreAntialiasing();
-		}
-		else
-		if($this.is('#same-size')){
+		} else
+		if ($this.is('#same-size')) {
 			resembleControl.scaleToSameSize();
-		}
-		else
-		if($this.is('#original-size')){
+		} else
+		if ($this.is('#original-size')) {
 			resembleControl.useOriginalSize();
-		}
-		else
-		if($this.is('#pink')){
+		} else
+		if ($this.is('#pink')) {
 			resemble.outputSettings({
 				errorColor: {
 					red: 255,
@@ -138,9 +140,8 @@ $(function(){
 				}
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#yellow')){
+		} else
+		if ($this.is('#yellow')) {
 			resemble.outputSettings({
 				errorColor: {
 					red: 255,
@@ -149,44 +150,38 @@ $(function(){
 				}
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#flat')){
+		} else
+		if ($this.is('#flat')) {
 			resemble.outputSettings({
 				errorType: 'flat'
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#movement')){
+		} else
+		if ($this.is('#movement')) {
 			resemble.outputSettings({
 				errorType: 'movement'
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#flatDifferenceIntensity')){
+		} else
+		if ($this.is('#flatDifferenceIntensity')) {
 			resemble.outputSettings({
 				errorType: 'flatDifferenceIntensity'
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#movementDifferenceIntensity')){
+		} else
+		if ($this.is('#movementDifferenceIntensity')) {
 			resemble.outputSettings({
 				errorType: 'movementDifferenceIntensity'
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#opaque')){
+		} else
+		if ($this.is('#opaque')) {
 			resemble.outputSettings({
 				transparency: 1
 			});
 			resembleControl.repaint();
-		}
-		else
-		if($this.is('#transparent')){
+		} else
+		if ($this.is('#transparent')) {
 			resemble.outputSettings({
 				transparency: 0.3
 			});
@@ -194,34 +189,34 @@ $(function(){
 		}
 	});
 
-	(function(){
+	(function () {
 		var xhr = new XMLHttpRequest();
 		var xhr2 = new XMLHttpRequest();
 		var done = $.Deferred();
 		var dtwo = $.Deferred();
 
-		xhr.open('GET', 'demoassets/People.jpg', true);
+		xhr.open('GET', 'screenshots/iPhone6/hello.png', true);
 		xhr.responseType = 'blob';
-		xhr.onload = function(e) {
+		xhr.onload = function (e) {
 			done.resolve(this.response);
 		};
 		xhr.send();
 
-		xhr2.open('GET', 'demoassets/People2.jpg', true);
+		xhr2.open('GET', 'screenshots/nexus10/hello.png', true);
 		xhr2.responseType = 'blob';
-		xhr2.onload = function(e) {
+		xhr2.onload = function (e) {
 			dtwo.resolve(this.response);
 		};
 		xhr2.send();
 
-		$('#example-images').click(function(){
+		$('#example-images').click(function () {
 
-			$('#dropzone1').html('<img src="demoassets/People.jpg"/>');
-			$('#dropzone2').html('<img src="demoassets/People2.jpg"/>');
+			$('#dropzone1').html('<img src="screenshots/iPhone6/hello.png"/>');
+			$('#dropzone2').html('<img src="screenshots/nexus10/hello.png"/>');
 
-			$.when(done, dtwo).done(function(file, file1){
+			$.when(done, dtwo).done(function (file, file1) {
 				if (typeof FileReader === 'undefined') {
-					resembleControl = resemble('demoassets/People.jpg').compareTo('demoassets/People2.jpg').onComplete(onComplete);
+					resembleControl = resemble('screenshots/iPhone6/hello.png').compareTo('screenshots/nexus10/hello.png').onComplete(onComplete);
 				} else {
 					resembleControl = resemble(file).compareTo(file1).onComplete(onComplete);
 				}
